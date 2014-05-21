a1b1937624dcd88748b104a72079c35934b34245
James Logan Mayfield <jlmayfield.4@gmail.com>
2014-05-21 10:19:36 -0500

    v0.0.1 commit

--- oct-circuits/inst/pTrace.m
+++ oct-circuits/inst/pTrace.m
@@ -18,26 +18,29 @@
 ## Trace out the space of bits r in the state represented by 
 ## density matrix P.  
 ##
-## For n bit state, r is either [0,m] or [m,n) for 0<m<n
+## For n bit state, r is either [0,m], or [m,n) for 0<m<n
 
 ## Author: Logan Mayfield <lmayfield@monmouthcollege.edu>
 ## Keywords: States
 
 function Q = pTrace(r,P)
   numBits = log2(rows(P));
-
+  
   if( length(r) > numBits ) # too many bits? 
-    error("Trace out space too large");
-  elseif( length(r) == numBits ) # all the bits?
-    error("Did you mean a complete trace?"); 
-  elseif( !isequal(diff(r),ones(1,length(r))) && length(r) > 1 ) # not a sequence
-    error("Trace out space must be sequential");
-  elseif(r(1) != 0 && r(length(r)) != numBits-1 ) # not a high/low group
-    error("Trace space must be [0,k] or [k,n]");
+    error("Trace out space too large. Given [%d,%d]. Space is \
+				%%[%d,%d)", ...
+	  r(1),r(length(r)),0,numBits);
+  elseif( length(r) > 1 && !isequal(diff(r),ones(1,length(r)-1)) ) # not a sequence
+    error("Trace out space must be sequential and increasing");    
+  elseif( r(1) != 0 && r(length(r)) != numBits-1 )
+    error("Only regions beginning at least or greatest bit allowed");
   endif
 
   ## get sizes of subspaces
-  outSize = 2^(length(r)); #size of traced out space
+  outBits = length(r);
+  outSize = 2^(outBits); #size of traced out space
+
+  inBits = numBits-outBits;
   inSize = 2^(numBits-length(r)); #size of remaining space 
 
   Q = zeros(inSize,inSize); # allocate result space
@@ -45,9 +48,12 @@ function Q = pTrace(r,P)
   ## There's gotta be a better way to collect blocks and generalize
   ## to non-high/low groups
 
-  if( r(1) == 0 ) # low order bitspace
-    ## collect blocks
-    blocks = zeros(outSize,outSize,numel(P)/(outSize^2));  
+  if( length(r) == numBits ) #complete trace
+    Q = trace(P);
+  elseif( r(1) == 0 ) # low order bitspace
+    blocks = zeros(outSize,outSize,inSize^2);   #allocate space
+    
+    ## collect blocks 
     for i = 1:(inSize^2)
       row =  floor(((i-1)/inSize))+1; # row number in block matrix
       col =  i-((row-1)*inSize); # col number in block matrix
@@ -57,7 +63,7 @@ function Q = pTrace(r,P)
     endfor
     
     # trace out each block
-    q = zeros(1,size(blocks)(3));
+    q = zeros(1,inSize^2);
     for i = 1:length(q)
       q(i) = trace(blocks(:,:,i));
     endfor
@@ -66,7 +72,7 @@ function Q = pTrace(r,P)
 
   elseif( r(length(r)) == numBits-1 ) # high order bit space
     ## collect blocks
-    blocks = zeros(inSize,inSize,numel(P)/(inSize^2));  
+    blocks = zeros(inSize,inSize,outSize^2);  
     for i = 1:(outSize^2)
       row =  floor(((i-1)/outSize))+1; # row number in block matrix
       col =  i-((row-1)*outSize); # col number in block matrix
@@ -75,10 +81,11 @@ function Q = pTrace(r,P)
 			(col-1)*inSize+1:col*inSize);
     endfor
     
-    ## trace out 
+    ## trace out by summing diagonal blocks 
     for i = [1:outSize]
       Q = Q + blocks(:,:,((i-1)*outSize)+i);
     endfor
+
   endif
 
 endfunction
@@ -87,5 +94,8 @@ endfunction
 %! X = transpose(reshape([1:16]',4,4));
 %! low = [7,11;23,27];
 %! high = [12,14;20,22];
-%! assert(low,pTrace([0],X));
-%! assert(high,pTrace([1],X));
+%! assert(low,pTrace([0],X)); 
+%! assert(high,pTrace([1],X)); 
+%! assert(trace(X),pTrace(0:1,X)); 
+
+
