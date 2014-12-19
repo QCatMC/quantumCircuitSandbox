35fa22bb035e85899a5edcbefbb3e61b9e5a519a
James Logan Mayfield <jlmayfield.4@gmail.com>
2014-12-19 16:43:00 -0600

    UZERO with verified conjugates and pointers
    addinverse transforms the 2 column UZERO to 3 column
    uzero where the third column is the index of the conjugate.
    It also removes any rows that do not have a conjugate in
    the table.  Verifytable was updated to check that the
    sequence computes the matrix (after global phase is factored out)
    and that the conjugate row is, in fact a conjugate.

--- skalgodata/addinverses.m
+++ skalgodata/addinverses.m
@@ -5,6 +5,7 @@ function tab = addinverses(tname)
 
   load(tname);
   tab = cell(rows(UZERO),3);
+
   tab(:,1) = UZERO(:,1); # sequence
   tab(:,2) = UZERO(:,2); # matrix
   tab(:,3) = 0 ;   # index of conjugate
@@ -17,7 +18,7 @@ function tab = addinverses(tname)
     if( tab{k,3} == 0 ) # if no conj found yet
       for l = k:rows(tab) # search k and the rest
 
-        if( norm( SUin-tab{l,2} ) < 2^-30 ) #if conj
+        if( norm( SUin-tab{l,2} ) < 2^-30 )  #if conj
           ## set indexes
           tab{l,3} = k;
           tab{k,3} = l;
@@ -28,49 +29,26 @@ function tab = addinverses(tname)
 
   endfor
 
-  ## sequences without conjugates... which should be none!
-  noinv = find(cell2mat(tab(:,3))==0);
-  for k = noinv.'
-    r = cell(1,3);
-    r{3} = k;
-    newlen = rows(tab)+1;
-    tab{k,3} = newlen;
+  ## get the index for all operators with conjugate in the table
+  keepers = cell2mat(tab(:,3)) !=0;
+  numkeep = sum(keepers) ## report this
+  oldIdx = find(keepers); ## idx of keepers in old tab
+  newIdx = cumsum(keepers); ## idx of keepers in new tab
 
-    r{2} = tab{k,2}';
-
-    r{1} = cell(1,length(tab{k,1}));
-
-    for l = 1:length(tab{k,1})
-      r{1}{l} = adj(tab{k,1}{l});
-    endfor
-
-    tab(end+1,:) = r;
+  ## grab the keepers
+  tab = tab(oldIdx,:);
+  ## rewrite pointers to conjugates
+  for k = 1:rows(tab)
+    tab{k,3} = newIdx(tab{k,3});
   endfor
 
-  noinv = find(cell2mat(tab(:,3))==0);
-  assert(isempty(noinv));
-  assert(isequal((1:rows(tab)).',unique(cell2mat(tab(:,3)))));
+  ## double check pointers
+  assert(isequal((1:rows(tab))', ...
+                  sort(cell2mat(tab(:,3)))));
 
 
   global UZERO = tab;
   save(tname,"UZERO");
   clear -g UZERO;
-
-endfunction
-
-## Adjoint by name/string
-## this is a bit hacky... but should work...
-function s = adj(opstr)
-
-  if(length(opstr) == 2) #only U'->U
-    s = opstr(1);
-  else #length is 1. U->U', unless it's Hermitian
-    switch(opstr)
-      case {"H","X","Y","Z","I"}
-      s= opstr;
-      otherwise
-      s = [opstr,"'"];
-    endswitch
-  endif
 
 endfunction
--- skalgodata/verifytable.m
+++ skalgodata/verifytable.m
@@ -1,30 +1,36 @@
 
-## test the correctness of UZERO 
+## test the correctness of UZERO
 function errs = verifytable(tname)
 
   load(tname);
   tab = UZERO;
   clear -g UZERO;
-  
-  addpath ../oct-circuits/inst/;
-  
-  errs = zeros(1,rows(tab)); # error between computed seq and given SU
+
+  addpath ../qcs/inst/;
+
+  errs = zeros(rows(tab),2); # error between computed seq and given SU
 
   for k = 1:rows(tab)
     seq = tab{k,1}; # current sequence
     SU = tab{k,2}; # current SU(2) operator
-    
-    
+    SUinv = tab{tab{k,3},2}; ## the conjugate matrix for current SU
+
+
     seqU = eye(2); # computed operator from seq
     for l = 1:length(seq)
       seqU = seqU*eval(seq{l});
     endfor
+    ## SU2afy the operator
     seqU = sqrt(det(seqU))' * seqU;
-    
-    errs(k) = norm(SU-seqU);
+
+    errs(k,1) = norm(SU-seqU); # error bettween computed and table value
+    errs(k,2) = norm((SU*SUinv)-eye(2)); # error for conjugate
 
   endfor
 
-  off = find(errs > 2^(-50))
+  off = find(errs(:,1) > 2^(-40) );
+  numoff = length(off)
+  invoff = find(errs(:,2) > 2^(-40) );
+  numconjoff = length(invoff)
 
 endfunction
