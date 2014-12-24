7ca5de9b84d2b359459cbab5acb015cb4cbd115d
James Logan Mayfield <jlmayfield.4@gmail.com>
2014-12-24 09:03:50 -0600

    pTrace does vectors
    The pTrace function now accepts state vectors as well as density
    matrices.  When vectors are given, they're converted to density
    matrices, then traced out. The result is still a density matrix.
    Resolves #24.

--- qcs/inst/pTrace.m
+++ qcs/inst/pTrace.m
@@ -14,9 +14,9 @@
 ##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## Usage: Q = pTrace(r,P)
-## 
-## Trace out the space of bits r in the state represented by 
-## density matrix P.  
+##
+## Trace out the space of bits r in the state represented by
+## density matrix P.
 ##
 ## For n bit state, r is either [0,m], or [m,n) for 0<m<n
 
@@ -25,15 +25,20 @@
 
 function Q = pTrace(r,P)
   numBits = log2(rows(P));
-  
-  if( length(r) > numBits ) # too many bits? 
+
+  if( length(r) > numBits ) # too many bits?
     error("Trace out space too large. Given [%d,%d]. Space is \
 				%%[%d,%d)", ...
 	  r(1),r(length(r)),0,numBits);
   elseif( length(r) > 1 && !isequal(diff(r),ones(1,length(r)-1)) ) # not a sequence
-    error("Trace out space must be sequential and increasing");    
+    error("Trace out space must be sequential and increasing");
   elseif( r(1) != 0 && r(length(r)) != numBits-1 )
     error("Only regions beginning at least or greatest bit allowed");
+  endif
+
+  ## vector... convert to density matrix
+  if( columns(P) == 1 )
+    P = pureToDensity(P);
   endif
 
   ## get sizes of subspaces
@@ -41,19 +46,16 @@ function Q = pTrace(r,P)
   outSize = 2^(outBits); #size of traced out space
 
   inBits = numBits-outBits;
-  inSize = 2^(numBits-length(r)); #size of remaining space 
+  inSize = 2^(numBits-length(r)); #size of remaining space
 
   Q = zeros(inSize,inSize); # allocate result space
-  
-  ## There's gotta be a better way to collect blocks and generalize
-  ## to non-high/low groups
 
   if( length(r) == numBits ) #complete trace
     Q = trace(P);
   elseif( r(1) == 0 ) # low order bitspace
     blocks = zeros(outSize,outSize,inSize^2);   #allocate space
-    
-    ## collect blocks 
+
+    ## collect blocks
     for i = 1:(inSize^2)
       row =  floor(((i-1)/inSize))+1; # row number in block matrix
       col =  i-((row-1)*inSize); # col number in block matrix
@@ -61,7 +63,7 @@ function Q = pTrace(r,P)
       blocks(:,:,i) = P((row-1)*outSize+1:row*outSize, ...
 			(col-1)*outSize+1:col*outSize);
     endfor
-    
+
     # trace out each block
     q = zeros(1,inSize^2);
     for i = 1:length(q)
@@ -72,7 +74,7 @@ function Q = pTrace(r,P)
 
   elseif( r(length(r)) == numBits-1 ) # high order bit space
     ## collect blocks
-    blocks = zeros(inSize,inSize,outSize^2);  
+    blocks = zeros(inSize,inSize,outSize^2);
     for i = 1:(outSize^2)
       row =  floor(((i-1)/outSize))+1; # row number in block matrix
       col =  i-((row-1)*outSize); # col number in block matrix
@@ -80,8 +82,8 @@ function Q = pTrace(r,P)
       blocks(:,:,i) = P((row-1)*inSize+1:row*inSize, ...
 			(col-1)*inSize+1:col*inSize);
     endfor
-    
-    ## trace out by summing diagonal blocks 
+
+    ## trace out by summing diagonal blocks
     for i = [1:outSize]
       Q = Q + blocks(:,:,((i-1)*outSize)+i);
     endfor
@@ -94,8 +96,6 @@ endfunction
 %! X = transpose(reshape([1:16]',4,4));
 %! low = [7,11;23,27];
 %! high = [12,14;20,22];
-%! assert(low,pTrace([0],X)); 
-%! assert(high,pTrace([1],X)); 
-%! assert(trace(X),pTrace(0:1,X)); 
-
-
+%! assert(low,pTrace([0],X));
+%! assert(high,pTrace([1],X));
+%! assert(trace(X),pTrace(0:1,X));
