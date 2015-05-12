9780fd84268576b6ea6a9b4d25fc7bfde588a8e7
James Logan Mayfield <jlmayfield.4@gmail.com>
2015-05-12 12:55:01 -0500

    Fixed QIRcU compilation
    I forgot to reverse the order of the mathematical notation.
    U=ABC  is a circuit applying C then B then A.  Good times.
    Resolves #41

--- qcs/inst/@QIRcU/compile.m
+++ qcs/inst/@QIRcU/compile.m
@@ -36,27 +36,34 @@ function q = compile(this)
   else
     qseq = cell(); # the QIASM sequence
 
-    ## get phase and SU2 zyz params w/out computing matrix
-    [zyzps,gp] = params(this.op);
-
-    ## c-Ph(gp) for non SU(2) Us
-    if( gp != 0 )
-      qseq{end+1} = @QIASMsingle("Rn",this.ctrl,[gp,0,0,1,0]);
-      qseq{end+1} = @QIASMsingle("PhAmp",this.ctrl,[0,0,0,gp/2]);
+    ## get params by kicking things over to zyzparams
+    ## this probably misses some special cases
+    params = zeros(1,4);
+    if(length(get(this,"op")) == 1)
+      params = zyzparams(circ2mat(@QIRsingle(get(this,"op"){1},0),1));
+    else
+      params = zyzparams(circ2mat(@QIRsingle(get(this,"op"){1},0,get(this,"op"){2}),1));
     endif
 
-    ## currently not checking for special cases (lemma 5.4 and 5.5)
+    zyzps = params(1:3);
+    gp = params(4);
 
-    ## all U need A,CNot,B
-    qseq{end+1} = @QIASMsingle("ZYZ",this.tar,[zyzps(1),zyzps(2)/2,0,0]);
-    qseq{end+1} = @QIASMcNot(this.tar,this.ctrl);
-    qseq{end+1} = @QIASMsingle("ZYZ",this.tar,[0,-(zyzps(2))/2,-(zyzps(1)+zyzps(3))/2,0]);
+    ## currently not checking for special cases (lemma 5.4 and 5.5)
+    ## this is optional in some cases ... also not checking
+    qseq{end+1} = @QIASMsingle("ZYZ",this.tar,[(zyzps(3)-zyzps(1))/2,0,0,0]);
+
     ## some will need a second CNot... not checking this case
     qseq{end+1} = @QIASMcNot(this.tar,this.ctrl);
 
-    ## some will also need C
-    if( !iszero(zyzps(3)-zyzps(1)) )
-      qseq{end+1} = @QIASMsingle("ZYZ",this.tar,[(zyzps(3)-zyzps(1))/2,0,0,0]);
+    ## all U need A,CNot,B
+    qseq{end+1} = @QIASMsingle("ZYZ",this.tar,[0,-zyzps(2)/2,-(zyzps(1)+zyzps(3))/2,0]);
+    qseq{end+1} = @QIASMcNot(this.tar,this.ctrl);
+    qseq{end+1} = @QIASMsingle("ZYZ",this.tar,[zyzps(1),zyzps(2)/2,0,0]);
+
+    ## c-Ph(gp) for non SU(2) Us
+    if( abs(gp)  > 2^-32 )
+      qseq{end+1} = @QIASMsingle("Rn",this.ctrl,[gp,0,0,1,0]);
+      qseq{end+1} = @QIASMsingle("PhAmp",this.ctrl,[0,0,0,gp/2]);
     endif
 
     q = @QIASMseq(qseq);
@@ -65,116 +72,22 @@ function q = compile(this)
 
 endfunction
 
-## Compute the parameters given the op spec array...
-## {name} or {name,params}
-## z is zyz params
-## g is global phase
-function [z,g] = params(op)
-
-  oname = op{1};
-  z = zeros(1,3);
-  switch(oname)
-    ## No param ops
-    case "H"
-      z = [0,pi/2,pi]; ## is this a special case?
-      g = pi/2;
-    ## setup remainding QASM ops for special cases
-    case "Z"
-      z = [pi/2,0,pi/2];
-      g = pi/2;
-    case "Y"
-      z = [0,pi,0];
-      g = pi/2;
-    case "T"
-      z = [pi/8,0,pi/8];
-      g = pi/8;
-    case "T'"
-      z = [-pi/8,0,-pi/8];
-      g =-pi/8;
-    case "S"
-      z = [pi/4,0,pi/4];
-      g = pi/4;
-    case "S'"
-      z = [-pi/4,0,-pi/4];
-      g = -pi/4;
-
-    ##  parameterized ops
-    case "PhAmp"
-      ## convert phamp -> zyz
-      z = [op{2}(3),2*op{2}(1),op{2}(2)];
-      if(length(op{2}) == 4)
-	g = op{2}(4);
-      else
-	g = 0;
-      endif
-
-    case "ZYZ"
-      z = op{2}(1:3);
-      if(length(op{2}) == 4)
-	g = op{2}(4);
-      else
-	g = 0;
-      endif
-
-
-    case "Rn"
-      ## convert Rn -> zyz
-
-      if(length(op{2}) == 5)
-	g = op{2}(5);
-      else
-	g = 0;
-      endif
-
-      n = op{2}(2:4);
-      a = op{2}(1);
-
-      if( norm([0,0,1]-n) <= 2^(-50) )#Rz/Diagonal matrix
-	z = [a/2,0,a/2]; ## special case
-      elseif( norm([0,1,0]-n) <= 2^(-50) ) #Ry
-	z = [0,a,0]; ## special case
-      elseif( norm([1,0,0]-n) <= 2^(-50) )#Rx
-	z = [-pi/2,a,pi/2];
-      elseif( abs(pi-a) < 2^(-60) && abs(n(3)) < 2^(-60)) #off-diagonal
-	z = [-2*acos(n(2)),pi,0];
-      else #everything else
-	## compute matrix and solve from there
-	X = [0,1;1,0]; Y=[0,-i;i,0];Z=[1,0;0,-1];
-	A = n(1)*X + n(2)*Y + n(3)*Z;
-	U = e^(-i*a/2*A);
-	z = [arg(U(2,1)*U(1,1)'),...
-	     2*acos(abs(U(1,1))),...
-	     arg(U(2,2)*U(2,1)')];
-      endif
-
-      ## ensure special case trigger when no Y rotation occurs
-      if( abs(z(2)) < 2^(-60) && abs(z(1)-z(3)) > 2^(-60) ) ##no Y, z's not equal
-	zrot = z(1) + z(3);
-	## evenly divide z-rotation between the two operators
-	z(1) = zrot/2;
-	z(2) = zrot/2;
-      endif
-
-  endswitch
-
-
-endfunction
-
 function b = iszero(dub)
-  b = abs(dub) < 2^(-60);
+  b = abs(dub) < 2^(-32);
 endfunction
 
 
 ## here we test by comparing the matrix form of the compiled operation
 ## to that of the QIR operation.  The result should be 'equivalent'
 %!test
-%! err = 2^(-40);
+%! err = 2^(-30);
 %! assert(eq(compile(@QIRcU(2,5,{"X"})),@QIASMcNot(2,5)));
-%! ops = {"H","Y","Z","S","T","T'","S'"};
+%! ops = {"Y","Z","S","T","T'","S'","H"};
 %! for op = ops
 %!    a = @QIRcU(0,1,op);
-%!    assert(operr(circ2mat(compile(a),2),circ2mat(a,2)) < err, op{1});
+%!    assert( operr(circ2mat(compile(a),2),circ2mat(a,2)) < err, op{1});
 %! endfor
+%!
 %! a = @QIRcU(0,1,{"PhAmp",[pi/3,pi/3,pi/3,pi/3]});
 %! assert(operr(circ2mat(compile(a),2),circ2mat(a,2)) < err, op{1});
 %! a = @QIRcU(0,1,{"Rn",[pi/3,sqrt(1/3),sqrt(1/6),sqrt(1/2),pi/5]});
